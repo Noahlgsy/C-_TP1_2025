1538bef310883fc07fca9c1f0ffb11e837c330c8
ccatelin <noah.lgsy@gmail.com>
2025-12-08 13:39:32 +0100

    création classe Date

---
+++ C++_TP1/Date.h
@@ -0,0 +1,12 @@
+#pragma once
+class Date
+{
+private: 
+	int day; 
+	int month; 
+	int year; 
+public: 
+	Date(int _day, int _month, int _year)
+		: day(_day), month(_month), year(_year) {}
+};
+
--- C++_TP1/main.cpp
+++ C++_TP1/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "Date.h"
 
 int main()
 {
 	std::cout << "Hello, world !" << std::endl; 
+	Date d1(13, 12, 2025); 
 	return 0; 
 }
